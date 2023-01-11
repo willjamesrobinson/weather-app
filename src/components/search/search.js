9818893b5fc70ac27c4369d293a4849c0f468496
willjamesrobinson <willjamesr@gmail.com>
2023-01-12 10:31:15 +1100

    finished search bar

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,16 +1,26 @@
-
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL } from '../../api';
+import { GEO_API_URL, geoApiOptions } from '../../api';
 
 const Search = (onSearchChange) => {
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        return fetch('/cities', options)
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions
+            )
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`,
+                    }
+                })
+            }
+        })
         .catch(err => console.error(err));
     }
 
